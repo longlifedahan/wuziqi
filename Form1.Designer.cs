28148244be834d368f82fb96fd091b52eb41674a
1780729091@qq.com <yjxjby13!>
2020-10-09 09:14:38 +0800

    ...

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace 五子棋
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.初始化棋盘ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.初始化棋盘ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(154, 28);
+            // 
+            // 初始化棋盘ToolStripMenuItem
+            // 
+            this.初始化棋盘ToolStripMenuItem.Name = "初始化棋盘ToolStripMenuItem";
+            this.初始化棋盘ToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.初始化棋盘ToolStripMenuItem.Text = "初始化棋盘";
+            this.初始化棋盘ToolStripMenuItem.Click += new System.EventHandler(this.初始化棋盘ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "五子棋";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 初始化棋盘ToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 五子棋
+{
+    public partial class Form1 : Form
+    {
+        const int sizex = 400 + 20;
+        const int sizey = 400 + 50;
+        Image black = Properties.Resources.Black;
+        Image white = Properties.Resources.White;
+        Image back = Properties.Resources.background;
+        int[,] keyboard = new int[19, 19];//null=0，black=1；white=-1
+        bool blacknow = true;
+        bool start = false;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Size = new Size(sizex, sizey);
+        }
+        private void initkeyboard()
+        {
+            start = true;//开始
+            blacknow = true;
+            for (int i = 0; i < 19; i++)//初始化数字棋盘
+                for (int j = 0; j < 19; j++)
+                    keyboard[i, j] = 0;
+            for (int i = 0; i < 19; i++)//初始化图形化棋盘
+                for (int j = 0; j < 19; j++)
+                    drawbackground(i, j);
+            Graphics g = this.CreateGraphics();
+            g.DrawString("0", this.Font, Brushes.Black, 0, 0);
+            for (int i = 0; i < 19; i++)
+                g.DrawString((i + 1).ToString(), this.Font, Brushes.Black, (i + 1) * 20, 0);
+            for (int j = 0; j < 19; j++)
+                g.DrawString((j + 1).ToString(), this.Font, Brushes.Black, 0, (j + 1) * 20);
+        }
+
+        private void drawbackground(int i, int j)
+        {
+            Graphics g = this.CreateGraphics();
+            Point[] points = new Point[] { new Point(i * 20 + 20, j * 20 + 20), new Point(i * 20 + 40, j * 20 + 20), new Point(i * 20 + 20, j * 20 + 40) };
+            g.DrawImage(back, points);
+        }
+
+        private void 初始化棋盘ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            initkeyboard();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (start)
+            {
+                int i = e.X / 20;
+                int j = e.Y / 20;
+                Graphics g = this.CreateGraphics();
+                //MessageBox.Show(i + " " + j);
+                if (blacknow)//黑[1,1]...[19,19]
+                {
+                    if (keyboard[i - 1, j - 1] == 0)
+                    {
+                        keyboard[i - 1, j - 1] = 1;
+                        blacknow = false;
+                        Point[] points = new Point[] { new Point(i * 20, j * 20), new Point(i * 20 + 20, j * 20), new Point(i * 20, j * 20 + 20) };
+                        g.DrawImage(black, points);
+                    }
+                }
+                else//百
+                {
+                    if (keyboard[i - 1, j - 1] == 0)
+                    {
+                        keyboard[i - 1, j - 1] = -1;
+                        blacknow = true;
+                        Point[] points = new Point[] { new Point(i * 20, j * 20), new Point(i * 20 + 20, j * 20), new Point(i * 20, j * 20 + 20) };
+                        g.DrawImage(white, points);
+                    }
+                }
+                judge();//判断胜负
+            }
+            else
+            {
+                MessageBox.Show("请右键初始化棋盘开始游戏！");
+            }
+        }
+
+        private void judge()
+        {
+            int[,] a = keyboard;
+            for (int i = 0; i < 19; i++)
+            {
+                for (int j = 0; j < 19; j++)
+                {
+                    //左上
+                    if (start)
+                    {
+                        try
+                        {
+                            int x = a[i, j] + a[i - 1, j - 1] + a[i - 2, j - 2] + a[i - 3, j - 3] + a[i - 4, j - 4];
+                            if (x == 5) { MessageBox.Show("黑子赢"); start = false; }
+                            else if (x == -5) { MessageBox.Show("白子赢"); start = false; }
+                        }
+                        catch { }
+                        //右下
+                        try
+                        {
+                            int x = a[i, j] + a[i + 1, j + 1] + a[i + 2, j + 2] + a[i + 3, j + 3] + a[i + 4, j + 4];
+                            if (x == 5) { MessageBox.Show("黑子赢"); start = false; }
+                            else if (x == -5) { MessageBox.Show("白子赢"); start = false; }
+                        }
+                        catch { }
+                        //右上
+                        try
+                        {
+                            int x = a[i, j] + a[i + 1, j - 1] + a[i + 2, j - 2] + a[i + 3, j - 3] + a[i + 4, j - 4];
+                            if (x == 5) { MessageBox.Show("黑子赢"); start = false; }
+                            else if (x == -5) { MessageBox.Show("白子赢"); start = false; }
+                        }
+                        catch { }
+                        //左下
+                        try
+                        {
+                            int x = a[i, j] + a[i - 1, j + 1] + a[i - 2, j + 2] + a[i - 3, j + 3] + a[i - 4, j + 4];
+                            if (x == 5) { MessageBox.Show("黑子赢"); start = false; }
+                            else if (x == -5) { MessageBox.Show("白子赢"); start = false; }
+                        }
+                        catch { }
+                        //左
+                        try
+                        {
+                            int x = a[i, j] + a[i - 1, j] + a[i - 2, j] + a[i - 3, j] + a[i - 4, j];
+                            if (x == 5) { MessageBox.Show("黑子赢"); start = false; }
+                            else if (x == -5) { MessageBox.Show("白子赢"); start = false; }
+                        }
+                        catch { }
+                        //右
+                        try
+                        {
+                            int x = a[i, j] + a[i + 1, j] + a[i + 2, j] + a[i + 3, j] + a[i + 4, j];
+                            if (x == 5) { MessageBox.Show("黑子赢"); start = false; }
+                            else if (x == -5) { MessageBox.Show("白子赢"); start = false; }
+                        }
+                        catch { }
+                        //上
+                        try
+                        {
+                            int x = a[i, j] + a[i, j - 1] + a[i, j - 2] + a[i, j - 3] + a[i, j - 4];
+                            if (x == 5) { MessageBox.Show("黑子赢"); start = false; }
+                            else if (x == -5) { MessageBox.Show("白子赢"); start = false; }
+                        }
+                        catch { }
+                        //下
+                        try
+                        {
+                            int x = a[i, j] + a[i, j + 1] + a[i, j + 2] + a[i, j + 3] + a[i, j - +4];
+                            if (x == 5) { MessageBox.Show("黑子赢"); start = false; }
+                            else if (x == -5) { MessageBox.Show("白子赢"); start = false; }
+                        }
+                        catch { }
+                    }
+                }
+            }
+        }
+    }
+}
